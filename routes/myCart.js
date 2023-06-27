ffec14164e90074a879efb2db085b3a90f9bee07
Your name <Your email>
2023-06-27 16:41:03 +0200

    routerupdate

--- routes/myCart.js
+++ routes/myCart.js
@@ -1,22 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Trip = require('../models/trips')
-const MyCart = require('../models/myCart')
+const Trip = require("../models/trips");
+const MyCart = require("../models/myCart");
 
-router.post('/',(req,res)=>{
-    const newCart = new MyCart ({
-        trip : req.body.tripId,
-        isPurchased : false})
-    newCart.save().then(data=>{
-        res.json({newTrip : data})
-    })
+router.post("/", (req, res) => {
+  const newCart = new MyCart({
+    trip: req.body.tripId,
+    isPurchased: false,
+  });
+  newCart.save().then((data) => {
+    res.json({ newTrip: data });
+  });
 });
 
-router.delete('/',(req,res)=>{
-    MyCart.deleteOne({id : req.body.myCartId})
-    .then(data=>{
-        res.json({allTrips : data})
-    })
+router.delete("/", (req, res) => {
+  MyCart.deleteOne({ id: req.body.myCartId }).then((data) => {
+    res.json({ allTrips: data });
+  });
+});
+
+router.post("/purchase", (req, res) => {
+  MyCart.updateOne({
+    id: req.body.myCartId,
+    isPurchased: true,
+  });
 });
 
 module.exports = router;
